37d1019f40d89121a62a128d1f7edf097e2d1507
clementwa0 <clementwa01@gmail.com>
2025-08-06 16:58:01 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Assign values to these four variables
+        //variable = reusable container for a value
+        /*  primitive --> value stored directly to the memory (stack)
+                      --> int double char boolean
+        */
+        /* Reference --> memory address (stack) the points to the (heap)
+                     --> String, array, object
+        */
+
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+
+        System.out.println(one + "\n" + two + "\n" + three + "\n" + four);
+    }
+}
